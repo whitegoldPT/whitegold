bbbb03c37a92e0e0bb925233bf370d041ff32589
whitegoldPT <whitegold.pomotech@gmail.com>
2026-08-11 18:59:42 +0300

    Delete pos_access_user_employee directory

--- pos_access_user_employee/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import pos_config
-from . import pos_access
-from . import pos_session
-from . import pos_category
-from . import pos_access_employee
-from . import pos_config_employee
-from . import res_config_settings_employee
--- pos_access_user_employee/models/pos_access_employee.py
+++
@@ -1,6 +0,0 @@
-from odoo import models,fields
-
-class PosAccess(models.Model):
-    _inherit = 'pos.access'
-
-    employee_ids = fields.Many2many('hr.employee', 'pos_access_hr_employee_rel', 'pos_access_id', 'employee_id')
