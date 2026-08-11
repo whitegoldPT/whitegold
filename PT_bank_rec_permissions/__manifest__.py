1994282f1ab7fe9a5a28275c4cd8f745d65a929b
whitegoldPT <whitegold.pomotech@gmail.com>
2026-08-11 19:46:27 +0300

    Add files via upload

---
+++ PT_bank_rec_permissions/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "PT Bank Reconciliation Security",
+    "version": "18.0.1.0.0",
+    "author": "Pomo Tech",
+    "category": "Accounting",
+    "depends": ["account", "account_accountant"],
+    "data": [
+        "security/security.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
