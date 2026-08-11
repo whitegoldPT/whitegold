ec5546aa99bef089f5b29394a72813ff6e3ea44b
whitegoldPT <whitegold.pomotech@gmail.com>
2026-08-11 19:02:24 +0300

    Delete sensible_pos_access_rights_employee directory

--- sensible_pos_access_rights_employee/__init__.py
+++
@@ -1,4 +0,0 @@
-# Powered by Sensible Consulting Services
-# -*- coding: utf-8 -*-
-# © 2025 Sensible Consulting Services (<https://sensiblecs.com/>)
-from . import models
--- sensible_pos_access_rights_employee/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Powered by Sensible Consulting Services
-# -*- coding: utf-8 -*-
-# © 2025 Sensible Consulting Services (<https://sensiblecs.com/>)
-from . import sbl_hr_employee_base
-from . import sbl_hr_employee
